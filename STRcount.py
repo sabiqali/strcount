1d188d548133c5eef90a72c6bb8d8f09e2ac3f7d
sabiqali <sabiq.work@gmail.com>
2022-07-14 17:47:10 -0400

    made changes to wrapper script

--- STRcount.py
+++ STRcount.py
@@ -14,43 +14,86 @@ parser.add_argument('--write-non-spanned', action='store_true', default=False, h
 parser.add_argument('--repeat_orientation', help='the orientation of the repeat string. + or -', required=False, default="+")
 parser.add_argument('--prefix_orientation', help='the orientation of the prefix, + or -', required=False, default="+")
 parser.add_argument('--suffix_orientation', help='the orientation of the suffix, + or -', required=False, default="+")
-parser.add_argument('--alignment_options', help='the alignment options', required=False, default="")
 parser.add_argument('--cleanup', help='do you want to clean up the temporary file?', required=False, default="yes")
 parser.add_argument('--output_directory', help='the output directory for all output and temporary files', required=False, default="./")
+parser.add_argument('--multiseed-DP', help='Aligner option', required=False, default="./")
+parser.add_argument('--precise-clipping', help='Aligner option: use arg as the identity threshold for a valid alignment.', required=False, default="./")
 args = parser.parse_args()
 
 ref = args.reference
 reads = args.fastq
 config_file = args.config
 min_id = args.min_identity
+min_align = args.min_aligned_fraction
 span = args.write_non_spanned
 rep_orientation = args.repeat_orientation
 pre_orientation = args.prefix_orientation
 suf_orientation = args.suffix_orientation
-opts = args.alignment_options
 cleanup_flag = 0 if args.cleanup == "no" else 1
 out_dir = args.output_directory
 out_file = args.output
+seed = args.multiseed_DP
+id_thres = args.precise_clipping
 
 cwd = os.getcwd()
 
+if min_id:
+    min_id_arg = f"--min-identity {min_id}"
+else:
+    min_id_arg = ""
+
+if span:
+    span_arg = f"--write-non-spanned {span}"
+else:
+    span_arg = ""
+
+if min_align:
+    min_align_arg = f"--min-aligned-fraction {min_align}"
+else:
+    min_align_arg = ""
+
+if rep_orientation:
+    rep_orientation_arg = f"--repeat_orientation {rep_orientation}"
+else:
+    rep_orientation_arg = ""
+
+if pre_orientation:
+    pre_orientation_arg = f"--prefix_orientation {pre_orientation}"
+else:
+    pre_orientation_arg = ""
+
+if suf_orientation:
+    suf_orientation_arg = f"--suffix_orientation {suf_orientation}"
+else:
+    suf_orientation_arg = ""
+
+if seed:
+    seed_arg = f"--multiseed-DP {seed}"
+else:
+    seed_arg = ""
+
+if id_thres:
+    id_thres_arg = f"--precise-clipping {id_thres}"
+else:
+    id_thres_arg = ""
+
 create_tmp_folder = os.system(f"mkdir {out_dir}tmp")
 
-str_graph_generator = os.system(f"python ./scripts/genome_str_graph_generator.py --ref {ref} --config {config_file} > {out_dir}tmp/genome_str_graph.gfa")
+str_graph_generator = os.system(f"python ./scripts/genome_str_graph_generator.py --ref {ref} --config {config_file} {rep_orientation_arg} {pre_orientation_arg} {suf_orientation_arg} > {out_dir}tmp/genome_str_graph.gfa")
 
 if str_graph_generator == 0:
     print("STR Reference Graph has been generated")
 else: 
     print("error::Error while Generating STR genome graph")
 
-ga_align = os.system(f"GraphAligner -g {out_dir}tmp/genome_str_graph.gfa -f {reads} -a {out_dir}tmp/alignment.gaf -x vg {opts}")
+ga_align = os.system(f"GraphAligner {seed_arg} {id_thres_arg} -g {out_dir}tmp/genome_str_graph.gfa -f {reads} -a {out_dir}tmp/alignment.gaf -x vg")
 
 if ga_align == 0:
     print("Reads aligned to Reference Graph")
 else:
     print("error::Error in aligning the reads to the reference graph")
 
-parse_gaf = os.system(f"python ./scripts/parse_gaf.py --input {out_dir}tmp/genome_str_graph.gfa > {out_dir}{out_file}")
+parse_gaf = os.system(f"python ./scripts/parse_gaf.py --input {out_dir}tmp/genome_str_graph.gfa {min_id_arg} {min_align_arg} {span_arg} > {out_dir}{out_file}")
 
 if parse_gaf == 0:
     print("A read wise count has been generated")
