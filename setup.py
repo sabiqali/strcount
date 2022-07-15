007bb257ab3b3fadbc4bb4627f72d1616912e05a
sabiqali <sabiq.work@gmail.com>
2022-07-15 12:04:22 -0400

    added command line usage

--- setup.py
+++ setup.py
@@ -7,27 +7,29 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+VERSION = '0.0.5'
 DESCRIPTION = 'A package to count the number of repeats in a Short Tandem Repeat Expansion from long reads.'
 LONG_DESCRIPTION = long_description
 # Setting up
 setup(
     name="STRcount",
-    version="0.0.2",
+    version="0.0.5",
     author="Sabiq Chaudhary",
     author_email="<sabiq.work@gmail.com>",
     description="A package to count the number of repeats in a Short Tandem Repeat Expansion from long reads.",
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=[
-        'pysam'
-    ],
+    install_requires=requirements,
     keywords=['python', 'STR', 'Repeats', 'Tandem Repeats'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-    ]
+    ],
+    scripts=['src/STRcount/STRcount.py']
 )
