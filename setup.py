fd161c3ed2a1c4898107b8bbb019c2a5373fe59f
sabiqali <sabiq.work@gmail.com>
2022-07-15 14:04:12 -0400

    Deployed and working

--- setup.py
+++ setup.py
@@ -7,23 +7,23 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-with open('requirements.txt') as f:
-    requirements = f.readlines()
+#with open('requirements.txt') as f:
+#    requirements = f.readlines()
 
-VERSION = '0.0.6'
+VERSION = '0.1.1'
 DESCRIPTION = 'A package to count the number of repeats in a Short Tandem Repeat Expansion from long reads.'
 LONG_DESCRIPTION = long_description
 # Setting up
 setup(
     name="STRcount",
-    version="0.0.6",
+    version="0.1.1",
     author="Sabiq Chaudhary",
     author_email="<sabiq.work@gmail.com>",
     description="A package to count the number of repeats in a Short Tandem Repeat Expansion from long reads.",
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=['pysam'],
     keywords=['python', 'STR', 'Repeats', 'Tandem Repeats'],
     entry_points ={
             'console_scripts': [
@@ -36,5 +36,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    scripts=['src/STRcount/STRcount.py']
+    scripts=['src/STRcount/STRcount.py','src/STRcount/genome_str_graph_generator.py','src/STRcount/parse_gaf.py']
 )
