f32e2f2ced6fedcde07612cec1effa0737991c46
sabiqali <sabiq.work@gmail.com>
2022-07-15 11:27:19 -0400

    build ready

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
-
+VERSION = '0.0.2'
+DESCRIPTION = 'A package to count the number of repeats in a Short Tandem Repeat Expansion from long reads.'
+LONG_DESCRIPTION = long_description
 # Setting up
 setup(
     name="STRcount",
-    version=0.0.1,
+    version="0.0.2",
     author="Sabiq Chaudhary",
     author_email="<sabiq.work@gmail.com>",
     description="A package to count the number of repeats in a Short Tandem Repeat Expansion from long reads.",
@@ -22,16 +21,11 @@ setup(
     long_description=long_description,
     packages=find_packages(),
     install_requires=[
-        'argparse', 
-        'logging', 
-        'os',
-        'pysam',
-        'sys',
-        'wheel'
+        'pysam'
     ],
     keywords=['python', 'STR', 'Repeats', 'Tandem Repeats'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
